3fe672d42a66d1e542f6a1107e67069dcbfe4922
Calla <maracondo@aol.com>
2016-03-30 14:31:11 -0400

    Deleting line

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -127,7 +127,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
   //var message = won ? "You win!" : "Game over!";
-  var message = won ? "You win! Password: lemonlyman" : "Game over!";
+  var message = won ? "Password: lemonlyman" : "Game over!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
