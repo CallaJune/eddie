f2c7cf61ba370cf28a1bc9969b55cd44cbba4b39
Calla <maracondo@aol.com>
2016-03-30 14:30:10 -0400

    Changed win message and deleted unecessary directory

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,8 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  //var message = won ? "You win!" : "Game over!";
+  var message = won ? "You win! Password: lemonlyman" : "Game over!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
